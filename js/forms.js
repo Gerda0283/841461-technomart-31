b0e42e2896b7e1427ac84144aeced3d3c8efc8a8
Gerda0283 <gerda0283@gmail.com>
2021-07-09 18:11:05 +0300

    Сетка на гридах для внутренней страницы
    Не работает шрифт PT Sans  с жирностью 700. Может где-то ошиблась в одной букве  / знаке / пробеле. Не могу найти. Проверила на доступность и переполнение.  Скрипт сделала пока только на форму обратной связи (кнопка под картой. Если где заметите какой-то баг, маякните пожалуйста

---
+++ js/forms.js
@@ -0,0 +1,60 @@
+/* Feedback Form */
+
+const link = document.querySelector(".get-in-touch-link");
+const popup = document.querySelector(".modal-feedback");
+const close = document.querySelector(".closed");
+const feedbackForm = popup.querySelector(".feedback-form");
+const submitForm = popup.querySelector("[type=submit]");
+const name = popup.querySelector("[value=name]");
+const email = popup.querySelector("[value=email]");
+const message = popup.querySelector("[value=message]");
+
+let isStorageSupport = true;
+let storage = "";
+
+try {
+storage = localStorage.getItem("name");
+} catch (err) {
+isStorageSupport = false;
+}
+
+link.addEventListener("click", function (evt) {
+evt.preventDefault();
+popup.classList.add("modal-feedback-show");
+
+if (storage) {
+  name.value = storage;
+  email.focus();
+} else {
+  name.focus();
+}
+});
+
+close.addEventListener("click", function (evt) {
+evt.preventDefault();
+popup.classList.remove("modal-feedback-show");
+popup.classList.remove("modal-error");
+});
+
+submitForm.addEventListener("submit", function (evt) {
+if (!name.value || !email.value || !message.value) {
+  evt.preventDefault();
+  popup.classList.remove("modal-error");
+  popup.offsetWidth = popup.offsetWidth;
+  popup.classList.add("modal-error");
+} else {
+  if (isStorageSupport) {
+    localStorage.setItem("name", name.value);
+  }
+}
+});
+
+window.addEventListener("keydown", function (evt) {
+if (evt.keyCode === 27) {
+  if (popup.classList.contains("modal-feedback-show")) {
+    evt.preventDefault();
+    popup.classList.remove("modal-feedback-show");
+    popup.classList.remove("modal-error");
+  }
+}
+});
